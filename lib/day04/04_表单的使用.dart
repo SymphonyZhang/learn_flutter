2a338419e079a0a5b484c246604510bd41dceef7
yx.Zhang <yx.zhang@byteflyer.com>
2020-08-06 11:20:45 +0800

    表单的校验

--- lib/day04/04_表单的使用.dart
+++ lib/day04/04_表单的使用.dart
@@ -11,6 +11,7 @@ class _FormDemoState extends State<FormDemo> {
 
   void registerForm() {
     registerFormKey.currentState.save();
+    registerFormKey.currentState.validate(); //开启表单输入的各个校验
     print('usrename : $username     password : $password');
   }
 
@@ -29,6 +30,12 @@ class _FormDemoState extends State<FormDemo> {
             onSaved: (newValue) {
               this.username = newValue;
             },
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'username not be null';
+              }
+              return null;
+            },
           ),
           TextFormField(
             obscureText: true, //密码模式
@@ -39,6 +46,13 @@ class _FormDemoState extends State<FormDemo> {
             onSaved: (newValue) {
               this.password = newValue;
             },
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'password not be null';
+              }
+              return null;
+            },
+            //autovalidate: true, //开启当前校验
           ),
           SizedBox(
             height: 16,
